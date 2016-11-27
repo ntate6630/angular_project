ac76de580fc18962dfec4be4d27d01764652c9e2
Nick Tate <ntate6630@yahoo.co.uk>
2016-11-27 22:33:26 +0000

    Add js

--- js/service.js
+++ js/service.js
@@ -1,10 +1,9 @@
 angular.module('UserService', [])
-    .factory('UserAPIService', function($http) {
- 
-        UserAPIService = {
-            callAPI: function(url, data) {
-                return $http.post(url, data)
-            }
-        }
-        return UserAPIService;
-    });
+	.factory('UserAPIService', function($http) {
+		UserAPIService = {
+			callAPI: function(url, data) {
+				return $http.post(url, data);
+			}
+		};
+		return UserAPIService;
+	});
