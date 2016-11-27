ced5d439b30a75bb96b0b9a454ed3d582818d7c3
Nick Tate <ntate6630@yahoo.co.uk>
2016-11-27 23:39:13 +0000

    Add js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
-angular.module('TodoApp', ['ngRoute', 'RouteControllers', 'UserService', 'angular-storage']);
+angular.module('TodoApp', ['ngRoute',  'RouteControllers', 'UserService', 'angular-storage', 'TodoService']);
  
 angular.module('TodoApp').config(function($routeProvider) {
-    	$routeProvider.when('/', {
-        	templateUrl: 'templates/home.html',
-        	controller: 'HomeController'
-    	})
-    	.when('/accounts/register', {
-        		templateUrl: 'templates/register.html',
-        		controller: 'RegisterController'
-    	});
+ 
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+    .when('/accounts/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    })
+    .when('/todo', {
+        templateUrl: 'templates/todo.html',
+        controller: 'TodoController'
+    });
 });
